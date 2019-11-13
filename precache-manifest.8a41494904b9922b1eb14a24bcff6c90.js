620c70a5e0f2d4e69db72723027837e404b08d4d
seawinda <lekochka@gmail.com>
2019-11-13 18:36:56 +0300

    Updates

---
+++ precache-manifest.8a41494904b9922b1eb14a24bcff6c90.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31de1bd2c6229df2a643abfa009ad7bd",
+    "url": "/test_eltrale/index.html"
+  },
+  {
+    "revision": "40bc06f27d7e6cacdfe6",
+    "url": "/test_eltrale/static/css/main.9d90dc49.chunk.css"
+  },
+  {
+    "revision": "d713c3b17333a54fcfe8",
+    "url": "/test_eltrale/static/js/2.99a67d4a.chunk.js"
+  },
+  {
+    "revision": "40bc06f27d7e6cacdfe6",
+    "url": "/test_eltrale/static/js/main.4b806eb4.chunk.js"
+  },
+  {
+    "revision": "4501a1a6bf661b2f0af5",
+    "url": "/test_eltrale/static/js/runtime-main.bfc6d85d.js"
+  },
+  {
+    "revision": "20a0cad52afbf84dc51331545dad22c6",
+    "url": "/test_eltrale/static/media/arrow.20a0cad5.svg"
+  },
+  {
+    "revision": "de964d4fa80a5bbdea7e397913bf8a2e",
+    "url": "/test_eltrale/static/media/bg.de964d4f.jpg"
+  },
+  {
+    "revision": "b26b392e7a41c5558e5abc36f57f4ee1",
+    "url": "/test_eltrale/static/media/clock.b26b392e.svg"
+  },
+  {
+    "revision": "337bdad5948e2c2360fbe064bda71e69",
+    "url": "/test_eltrale/static/media/excu_bg.337bdad5.png"
+  },
+  {
+    "revision": "b8f10f9dba767e835fdc1ac208d084d8",
+    "url": "/test_eltrale/static/media/login.b8f10f9d.svg"
+  },
+  {
+    "revision": "081372e4b036e93cd9e8f4ce70b2348d",
+    "url": "/test_eltrale/static/media/logogo.081372e4.svg"
+  },
+  {
+    "revision": "daa523e60488bfad66d033b2a5a6484c",
+    "url": "/test_eltrale/static/media/star.daa523e6.svg"
+  }
+]);
